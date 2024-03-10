da7241ec74a3f62ce523ea296b779f89480d2165
Siddharth Jain <siddharthj1405@gmail.com>
2024-03-10 10:33:46 +0530

    chatbot UX and Javascript

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,28 +1,64 @@
+import React, { useState } from "react";
 import "./Chat.css";
 
 function Chat() {
+  const [userMessage, setUserMessage] = useState("");
+  const [inputHeight, setInputHeight] = useState("auto");
+  const [chatMessages, setChatMessages] = useState([]);
+
+  const handleInputChange = (e) => {
+    setUserMessage(e.target.value.trim());
+    setInputHeight(`${e.target.scrollHeight}px`);
+  };
+
+  const handleSendChat = () => {
+    const message = userMessage.trim();
+    if (!message) return;
+    setChatMessages((prevMessages) => [
+      ...prevMessages,
+      { content: message, type: "outgoing" },
+    ]);
+    setUserMessage(""); // Clear the input textarea
+    setInputHeight("auto"); // Reset input textarea height
+    generateResponse(message);
+  };
+
+  const generateResponse = (userMessage) => {
+    return "userMessage !!";
+  };
+
   return (
     <div className="chatBot">
       <header>
         <h2>Chatbot</h2>
       </header>
       <ul className="chatbox">
-        <li className="chat incoming">
-          <span>
-             
-            <img src="src/assets/logo-chatbot.png" alt="Logo" className="logo" />
-          </span>
-          <p>
-            Hi there <br></br>How can I help you today?
-          </p>
-        </li>
-        <li className="chat outgoing">
-          <p>Lorem ipsum dolor sit amet .</p>
-        </li>
+        {chatMessages.map((message, index) => (
+          <li key={index} className={`chat ${message.type}`}>
+            {message.type === "incoming" && (
+              <span>
+                <img
+                  src="src/assets/logo-chatbot.png"
+                  alt="Logo"
+                  className="logo"
+                />
+              </span>
+            )}
+            <p>{message.content}</p>
+          </li>
+        ))}
       </ul>
       <div className="chat-input">
-        <textarea placeholder="Enter a message..." required></textarea>
-        <span id="send-btn">&#10148;</span>
+        <textarea
+          placeholder="Enter a message..."
+          value={userMessage}
+          onChange={handleInputChange}
+          style={{ height: inputHeight }}
+          required
+        ></textarea>
+        <span id="send-btn" onClick={handleSendChat}>
+          &#10148;
+        </span>
       </div>
     </div>
   );
