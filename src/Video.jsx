535b7b7f0896efd0a64dfc99279a3a1447d46cad
Siddharth Jain <siddharthj1405@gmail.com>
2024-03-10 13:18:41 +0530

    Move separate emotion detector files to a folder

--- src/Video.jsx
+++ src/Video.jsx
@@ -3,22 +3,9 @@ import { useRef } from "react";
 import { useEffect } from "react";
 
 function Video() {
-    const videoRef = useRef(null);
-    useEffect(() => {
-      const startVideo = () => {
-        navigator.mediaDevices
-          .getUserMedia({ video: true })
-          .then((stream) => {
-            videoRef.current.srcObject = stream;
-          })
-          .catch((error) => console.error("Error accessing the webcam:", error));
-      };
-  
-      startVideo();
-    }, []);
   return (
-    <div className="liveVideo">
-      <video ref={videoRef} autoPlay className="video" />
+    <div className="video">
+      Webcam
     </div>
   );
 }
